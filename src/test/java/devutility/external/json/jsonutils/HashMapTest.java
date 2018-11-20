96f6fa9e15860261c76b9a1a8c9fc70870e50f03
Aldwin Su <fangyansu@163.com>
2018-11-20 15:56:48 +0800

    Add HashMapTest

---
+++ src/test/java/devutility/external/json/jsonutils/HashMapTest.java
@@ -0,0 +1,29 @@
+package devutility.external.json.jsonutils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import devutility.external.json.JsonUtils;
+import devutility.internal.test.BaseTest;
+import devutility.internal.test.TestExecutor;
+
+public class HashMapTest extends BaseTest {
+	@Override
+	public void run() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("asd", "123");
+		map.put("qwe", "456");
+
+		try {
+			println(JsonUtils.serialize(map));
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		TestExecutor.run(HashMapTest.class);
+	}
+}
